c6ae54682fd5f7dc07860eb7b7c2be16565873f2
MOHAMMED SIYAD <62105822+siyadmohammed@users.noreply.github.com>
2020-03-13 15:53:32 +0530

    Create to display prime number's in a given range.c

---
+++ to display prime number's in a given range.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+
+  {
+
+   int num1, num2, flag, i, j;
+
+ 
+
+  
+
+   printf("Enter two range(input integer numbers only):");  
+
+   scanf("%d %d", &num1, &num2);
+
+ 
+
+   printf("Prime numbers from %d and %d are:\n", num1, num2);
+
+   for(i=num1+1; i<num2; ++i)
+
+     {
+
+      flag=0;
+
+      for(j=2; j<=i/2; ++j)
+
+      {
+
+         if(i%j==0)
+
+         {
+
+            flag=1;
+
+            break;
+
+         }
+
+      }
+
+      if(flag==0)
+
+         printf("%d\n",i);
+
+      }
+
+    return 0;
+
+  }
