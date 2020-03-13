32d5c758cd7b24a1ef24ac4ddbecd7f071d91158
MOHAMMED SIYAD <62105822+siyadmohammed@users.noreply.github.com>
2020-03-13 15:32:10 +0530

    Update TOBDISPLAY FLOYD'S TRIANGLE.c

--- TOBDISPLAY FLOYD'S TRIANGLE.c
+++ TOBDISPLAY FLOYD'S TRIANGLE.c
@@ -4,24 +4,25 @@ main( )
 {
 
     int i, j, k = 1;
-
- 
-
     printf("floyds triangle is\n");
-
     for( i = 1; k <= 20; ++i )
 
     {
 
         for( j = 1; j <= i; ++j )
-
-	      printf( "%d ", k++ );	      printf( "\n\n" );
+        printf( "%d ", k++ );
+		      	
 
     }
 
     return 0;
 
 }
+
+ 
+
+
+    
 
 
 
